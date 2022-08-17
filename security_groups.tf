0acc67137bd1a1406fcb850b53b7ff3aab5e3f76
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2022-08-17 11:52:20 -0500

    new security features added

--- security_groups.tf
+++ security_groups.tf
@@ -21,10 +21,10 @@ resource "aws_default_security_group" "default-sg" {
     Name = "Default-sg"
   }
 
-  depends_on = [aws_vpc.informe_nube]
+  depends_on = [aws_vpc.SoftServe-vpc]
 }
 
-# Security Group
+# EC2 Security Group
 resource "aws_security_group" "SoftServe-sg" {
   name        = "SoftServe-sg"
   description = "Security group for EC2 instances"
@@ -45,7 +45,7 @@ resource "aws_security_group_rule" "http" {
   security_group_id = aws_security_group.SoftServe-sg.id
   description       = "Allow connections to HTTP port from any IP"
 
-  depends_on = [aws_security_group.SoftServe-sg.id]
+  depends_on = [aws_security_group.SoftServe-sg]
 }
 
 resource "aws_security_group_rule" "ssh" {
@@ -58,5 +58,35 @@ resource "aws_security_group_rule" "ssh" {
   security_group_id = aws_security_group.SoftServe-sg.id
   description       = "Allow connections to the SSH port from any IP"
 
-  depends_on = [aws_security_group.servidor_web]
+  depends_on = [aws_security_group.SoftServe-sg]
+}
+
+# NLB Security group
+# - Enable external access to TCP ports 80
+# - Allows access from the Load Balancer to the Security Group of the EC2 instances
+resource "aws_security_group" "sg_lb" {
+  name        = "sg_lb"
+  description = "ALB - Security Group"
+  vpc_id      = aws_vpc.SoftServe-vpc.id
+
+  # INGRESS RULES
+  ingress {
+    from_port   = "80"
+    to_port     = "80"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Habilita el acceso HTTP al puerto 80"
+  }
+
+  # EGRESS RULES
+  egress {
+    from_port   = "0"
+    to_port     = "0"
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "sg_lb"
+  }
 }
