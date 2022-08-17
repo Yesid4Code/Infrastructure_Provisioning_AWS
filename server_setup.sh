468961b4fc8d3c33195c9b8bb3b218aca08aeb9d
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2022-08-17 00:58:09 -0500

    Script to setup ac2 instances

---
+++ server_setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo yum update -y
+sudo yum install httpd -y
+sudo systemctl enable httpd
+sudo systemctl start httpd
+
+echo "<h1>Hello World from EC2 $(hostname -f)</h1>" > /var/www/html/index.html
