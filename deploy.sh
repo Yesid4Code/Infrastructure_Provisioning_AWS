ad6c1bff4fbb318b2f161ddfda2f2c0e049f5d24
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2022-08-17 13:29:22 -0500

    Bash scripting to deploy the provisioning (#1)

---
+++ deploy.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+
+dir='.terraform'
+if [ -d $dir ]; then
+    terraform destroy --auto-approve
+    rm -rfv .terraform
+fi
+
+file='.terraform.lock.hcl'
+if [ -f $file ]; then
+    rm -rfv .terraform.lock.hcl
+fi
+
+file='terraform.tfstate'
+if [ -f $file ]; then
+    rm -rfv terraform.tfstate
+fi
+
+file='terraform.tfstate.backup'
+if [ -f $file ]; then
+    rm -rfv terraform.tfstate.backup
+fi
+
+file='tfplan'
+if [ -f $file ]; then
+    rm -rfv tfplan
+fi
+
+terraform init
+terraform fmt
+terraform validate
+
+terraform plan -out=tfplan
+terraform apply --auto-approve tfplan
