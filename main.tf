e443b3709b7d7250c7f2d7d381ba69f433e17da0
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2022-08-17 03:17:10 -0500

    split code and tfm applied

--- main.tf
+++ main.tf
@@ -1,62 +1,19 @@
-# empieza
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-
-  required_version = ">=1.2.0"
-}
-
+# Main
 provider "aws" {
   region = "us-east-2"
 }
 
-# Create a VPC
-resource "aws_vpc" "SoftServe-vpc" {
-  cidr_block = "10.0.0.0/16"
-  instance_tenancy = "default"
-
-  tags = {
-    Name = "SoftServe-vpc"
-  }
-}
-
-# Create 2 subnets
-resource "aws_subnet" "SoftServe-2a" {
-  vpc_id = aws_vpc.SoftServe-vpc.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "us-west-2a"
-
-  tags = {
-    Name = "SoftServe-2a"
-  }
-}
-
-resource "aws_subnet" "SoftServe-2b" {
-  vpc_id = aws_vpc.SoftServe-vpc.id
-  cidr_block = "10.0.2.0/24"
-  availability_zone = "us-west-2b"
-
-  tags = {
-    Name = "SoftServe-2b"
-  }
-}
-
-  # ---------------
 # Create the AMI
 data "aws_ami" "ubuntu" {
   most_recent = true
 
   filter {
-    name = "name"
+    name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
 
   filter {
-    name = "virtualization-type"
+    name   = "virtualization-type"
     values = ["hvm"]
   }
 
@@ -65,13 +22,13 @@ data "aws_ami" "ubuntu" {
 
 # Create 2 EC2 instances with a server
 resource "aws_instance" "web_server_ec2_1" {
-  ami = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [ "aws_security_group" ] ## PENDIENTE
-  subnet_id = aws_subnet.soft-test-subnet-1a
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["aws_security_group"] ## PENDIENTE
+  subnet_id              = aws_subnet.SoftServe-2a.id
 
-  key_name = "first_ec2"
-  user_data = "${file("install_apache.sh")}"
+  key_name  = "first_ec2"
+  user_data = file("install_apache.sh")
 
 
   tags = {
@@ -80,13 +37,13 @@ resource "aws_instance" "web_server_ec2_1" {
 }
 
 resource "aws_instance" "web_server_ec2_2" {
-  ami = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [ "aws_security_group" ] ## PENDIENTE
-  subnet_id = aws_subnet.soft-test-subnet-1a
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["aws_security_group"] ## PENDIENTE
+  subnet_id              = aws_subnet.SoftServe-2b
 
-  key_name = "first_ec2"
-  user_data = "${file("install_apache.sh")}"
+  key_name  = "first_ec2"
+  user_data = file("install_apache.sh")
 
 
   tags = {
