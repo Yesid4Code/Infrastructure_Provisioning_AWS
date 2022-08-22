c071f193e279100a8b95c8070063c3740e502ac4
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2022-08-22 17:42:03 -0500

    VPC, simplifying the creation. Load Balancer and NACL removed, these services will be implemented in later versions of the architecture.

--- vpc.tf
+++ vpc.tf
@@ -1,17 +1,53 @@
 # VPC
-# Create a VPC
-resource "aws_vpc" "SoftServe-vpc" {
+resource "aws_vpc" "SoftServe_vpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
   tags = {
-    Name = "SoftServe-vpc"
+    Name = "SoftServe_vpc"
+  }
+}
+
+# Internet Gataway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.SoftServe_vpc.id
+
+  depends_on = [
+    aws_vpc.SoftServe_vpc
+  ]
+
+  lifecycle {
+    prevent_destroy = false
+  }
+
+  tags = {
+    "Name" = "Internet Gateway"
+  }
+}
+
+# Route tables to route traffic for Public Subnet
+resource "aws_route_table" "rt_main" {
+  vpc_id = aws_vpc.SoftServe_vpc.id
+
+  # route { cidr_block = 10.0.0.0/16 target local}
+  route {
+    cidr_block = "0.0.0.0/0" # To send all traffic to all internet
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  route {
+    ipv6_cidr_block = "::/0"
+    gateway_id      = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    "Name" = "rt_main"
   }
 }
 
 # Create 2 subnets
-resource "aws_subnet" "SoftServe-2a" {
-  vpc_id            = aws_vpc.SoftServe-vpc.id
+resource "aws_subnet" "subnet_2a" {
+  vpc_id            = aws_vpc.SoftServe_vpc.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-2a"
 
@@ -20,8 +56,8 @@ resource "aws_subnet" "SoftServe-2a" {
   }
 }
 
-resource "aws_subnet" "SoftServe-2b" {
-  vpc_id            = aws_vpc.SoftServe-vpc.id
+resource "aws_subnet" "subnet_2b" {
+  vpc_id            = aws_vpc.SoftServe_vpc.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "us-east-2b"
 
@@ -30,84 +66,13 @@ resource "aws_subnet" "SoftServe-2b" {
   }
 }
 
-# Internet Gataway
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.SoftServe-vpc.id
-
-  tags = {
-    "Name" = "Internet Gateway"
-  }
-
-  lifecycle {
-    prevent_destroy = false
-  }
-
-  depends_on = [
-    aws_vpc.SoftServe-vpc
-  ]
+# Associate route table
+resource "aws_route_table_association" "_2a" {
+  subnet_id      = aws_subnet.subnet_2a.id
+  route_table_id = aws_route_table.rt_main.id
 }
 
-# Route tables to route traffic for Public Subnet
-resource "aws_route_table" "rt-public" {
-  vpc_id = aws_vpc.SoftServe-vpc.id
-
-  tags = {
-    "Name" = "rt-public"
-  }
-}
-
-# --------------------
-# Load Balancer
-resource "aws_lb" "balancing" {
-  name               = "balancing-lb"
-  internal           = false
-  load_balancer_type = "network"
-
-  # subnets = [aws_subnet.SoftServe-2a, aws_subnet.SoftServe-2b]
-
-  subnet_mapping {
-    subnet_id = aws_subnet.SoftServe-2a.id
-    #private_ipv4_address = aws_subnet.SoftServe-2a
-  }
-
-  subnet_mapping {
-    subnet_id = aws_subnet.SoftServe-2a.id
-    #private_ipv4_address  = aws_subnet.SoftServe-2b
-  }
-
-  #security_groups            = [aws_security_group.sg_lb.id]
-  enable_deletion_protection = true
-
-  tags = {
-    Environment = "production"
-  }
-}
-
-
-# --------------------
-# NACL -> traffic between instances and the LB
-resource "aws_network_acl" "main" {
-  vpc_id = aws_vpc.SoftServe-vpc.id
-
-  egress {
-    protocol   = "tcp"
-    rule_no    = 200
-    action     = "allow"
-    cidr_block = "10.3.0.0/18"
-    from_port  = 443
-    to_port    = 443
-  }
-
-  ingress {
-    protocol   = "tcp"
-    rule_no    = 100
-    action     = "allow"
-    cidr_block = "10.3.0.0/18"
-    from_port  = 80
-    to_port    = 80
-  }
-
-  tags = {
-    Name = "main"
-  }
+resource "aws_route_table_association" "_2b" {
+  subnet_id      = aws_subnet.subnet_2b.id
+  route_table_id = aws_route_table.rt_main.id
 }
