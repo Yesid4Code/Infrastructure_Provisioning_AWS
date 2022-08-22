49fe48f8d778edaa6b2c94189e4b2ae4152fd5c1
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2022-08-22 18:17:43 -0500

    Refactoring (#2)
    * VPC, simplifying the creation.
    Load Balancer and NACL removed, these services will be implemented in later versions of the architecture.
    
    * Server architecture.
    
    * Security groups
    NLB Security group removed.
    
    * variables to show

--- outputs.tf
+++ outputs.tf
@@ -1,47 +1,40 @@
 # VPC info.
-output "SoftServe-vpc_id" {
+output "SoftServe_vpc_id" {
   description = "The VPC ID"
-  value       = try(aws_vpc.SoftServe-vpc.id, "")
+  value       = try(aws_vpc.SoftServe_vpc.id, "")
 }
 
-output "SoftServe-vpc_CIDR" {
+output "SoftServe_vpc_CIDR" {
   description = "The IPv4 CIDR"
-  value       = try(aws_vpc.SoftServe-vpc.cidr_block, "")
+  value       = try(aws_vpc.SoftServe_vpc.cidr_block, "")
 }
 
-# Subnet 1 info.
-output "arn_instance_1" {
+output "arn_web_server_ec2_2a" {
   description = "The ARN of the instance"
-  value       = try(aws_instance.web_server_ec2_1.arn, "")
+  value       = try(aws_instance.web_server_ec2_2a.arn, "")
 }
 
-output "public_ip_instance_1" {
-  description = "The public IP Address assigned to the instance."
-  value       = try(aws_instance.web_server_ec2_1.public_ip, "")
-}
-
-# Subnet 1
-output "arn_instance_2" {
+output "arn_web_server_ec2_2b" {
   description = "The ARN of the instance"
-  value       = try(aws_instance.web_server_ec2_2.arn, "")
+  value       = try(aws_instance.web_server_ec2_2b.arn, "")
 }
 
-output "public_ip_instance_2" {
+output "public_ip_web_server_ec2_2a" {
   description = "The public IP Address assigned to the instance."
-  value       = try(aws_instance.web_server_ec2_2.public_ip, "")
+  value       = try(aws_instance.web_server_ec2_2a.public_ip, "")
 }
 
-output "private_ip_instance_1" {
+output "public_ip_web_server_ec2_2b" {
+  description = "The public IP Address assigned to the instance."
+  value       = try(aws_instance.web_server_ec2_2b.public_ip, "")
+}
+
+output "private_ip_web_server_ec2_2a" {
   description = "The private IP addres for the subnet 1"
-  value       = try(aws_instance.web_server_ec2_1.private_ip, "")
+  value       = try(aws_instance.web_server_ec2_2a.private_ip, "")
 }
 
-output "private_ip_instance_2" {
+output "private_ip_web_server_ec2_2b" {
   description = "The private IP addres for the subnet 2"
-  value       = try(aws_instance.web_server_ec2_2.private_ip, "")
-}
-
-# LoadBalancer info.
-output "LoadBalancer_ip" {
-  description = "LoadBalancer IP"
+  value       = try(aws_instance.web_server_ec2_2b.private_ip, "")
 }
